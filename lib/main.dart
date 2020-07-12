5233ee9251de3534b8b744db8e08beb2bf339f11
Hanjaya Suryalim <hanjayasuryalim@gmail.com>
2020-07-12 16:33:05 +0700

    layout setting

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,31 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(DicePage());
+  runApp(MaterialApp(
+    home: Scaffold(
+        backgroundColor: Colors.grey.shade100,
+        appBar: AppBar(
+          backgroundColor: Colors.grey.shade900,
+          title: Center(
+            child: Text(
+              'Rollin Dice App',
+              style: TextStyle(
+                fontFamily: 'IndieFlower',
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+        body: DicePage()),
+  ));
 }
 
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Colors.grey.shade100,
+    );
   }
 }
